77e0dcbda8d76277fa2c7abf568d9bed94ad0475
jaya kumaar <mjk34012@gmail.com>
2025-01-02 21:45:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Use HashRouter for GitHub Pages
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AdminPage from "./pages/AdminPage";
 import UserPage from "./pages/UserPage";
 import ReportingPage from './pages/ReportingPage';
-import CompanyManagement from './components/CompanyManagement'; // Import CompanyManagement
-import CommunicationMethodManagement from './components/CommunicationMethodManagement'; // Import CommunicationMethodManagement
+import CompanyManagement from './components/CompanyManagement';
+import CommunicationMethodManagement from './components/CommunicationMethodManagement';
 import './styles/App.css';
 
 const App = () => {
@@ -13,18 +13,14 @@ const App = () => {
     <div className="App">
       <Router>
         <Navbar />
+
         <Routes>
-          {/* Define routes */}
           <Route path="/" element={<AdminPage />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/user" element={<UserPage />} />
           <Route path="/reporting" element={<ReportingPage />} />
-
-          {/* Add route for Company Management and Communication Method Management */}
           <Route path="/company-management" element={<CompanyManagement />} />
           <Route path="/communication-method-management" element={<CommunicationMethodManagement />} />
-
-          {/* Fallback route for undefined paths */}
           <Route path="*" element={<h2>Page Not Found</h2>} />
         </Routes>
       </Router>
